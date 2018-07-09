b9d67e40fd62614cc7990de25c03090e7076522b
KamilWro <kamil.breczko@gmail.com>
2018-07-09 22:13:06 +0200

    delete unnecessary class

--- dependency-injection/src/main/java/pl/insert/framework/adnotations/transactional/Transactional.java
+++
@@ -1,7 +0,0 @@
-package pl.insert.framework.adnotations.transactional;
-
-import pl.insert.framework.transactional.TransactionalPropagation;
-
-public @interface Transactional {
-    TransactionalPropagation propagation();
-}
