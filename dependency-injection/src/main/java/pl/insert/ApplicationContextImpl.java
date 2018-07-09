9ad1705b153d09699f11c907fe26dc98769acb30
KamilWro <kamil.breczko@gmail.com>
2018-07-09 09:42:43 +0200

    add lazy creation of beans

--- dependency-injection/src/main/java/pl/insert/ApplicationContextImpl.java
+++ dependency-injection/src/main/java/pl/insert/ApplicationContextImpl.java
@@ -3,6 +3,7 @@ package pl.insert;
 import org.reflections.Reflections;
 import pl.insert.adnotations.Bean;
 import pl.insert.adnotations.Inject;
+import pl.insert.adnotations.PersistenceContext;
 import pl.insert.adnotations.components.Repository;
 import pl.insert.adnotations.components.Service;
 import pl.insert.entitymanager.EntityManagerHandler;
@@ -11,61 +12,40 @@ import pl.insert.util.ClassUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ApplicationContextImpl implements ApplicationContext {
-
-    private static Map<Class, Object> applicationScope = new HashMap<>();
     private static Map<Class, Class> components = new HashMap<>();
+    private Map<Class, Object> applicationScope = new HashMap<>();
+    private Map<Class, Method> beans = new HashMap<>();
+    private Optional<Object> appConfiguration = Optional.empty();
 
     static {
         Reflections reflections = new Reflections("");
         Set<Class<?>> types = reflections.getTypesAnnotatedWith(Service.class);
         types.addAll(reflections.getTypesAnnotatedWith(Repository.class));
 
-        for (Class<?> implementationClass : types) {
-            for (Class iface : implementationClass.getInterfaces()) {
-                components.put(iface, implementationClass);
-            }
-            components.put(implementationClass, implementationClass);
-        }
+        types.forEach(type -> {
+            Arrays.stream(type.getInterfaces()).forEach(iface -> components.put(iface, type));
+            components.put(type, type);
+        });
     }
 
     public ApplicationContextImpl() {
     }
 
-    public ApplicationContextImpl(Class<?> clazz) {
-        Object configuration = ClassUtil.newInstance(clazz);
-        Class<?> aClass = configuration.getClass();
-        Arrays.stream(aClass.getMethods()).filter(method -> method.isAnnotationPresent(Bean.class)).forEach(method -> System.out.print(method.getDeclaredAnnotations()));
+    public ApplicationContextImpl(Class<?> appConfiguration) {
+        this.appConfiguration = Optional.ofNullable(ClassUtil.newInstance(appConfiguration));
 
-        Arrays.stream(configuration.getClass().getMethods())
+        Arrays.stream(appConfiguration.getMethods())
                 .filter(method -> method.isAnnotationPresent(Bean.class))
-                .forEach(method -> createBeansFormConfiguration(method, configuration));
-    }
-
-    private void createBeansFormConfiguration(Method method, Object configuration) {
-        Class<?> returnType = method.getReturnType();
-
-        if (returnType.isAssignableFrom(EntityManagerFactory.class)) {
-            Object entityManagerProxy = getEntityManagerProxy(configuration, method);
-            applicationScope.put(EntityManager.class, entityManagerProxy);
-        }
-        Object bean = ClassUtil.invokeMethod(configuration, method);
-        injectDependencies(bean);
-        addBeans(returnType, bean);
-    }
-
-    private Object getEntityManagerProxy(Object configuration, Method method) {
-        EntityManagerFactory entityManagerFactory = (EntityManagerFactory) ClassUtil.invokeMethod(configuration, method);
-        EntityManagerHelper entityManagerHelper = new EntityManagerHelper(entityManagerFactory);
-        EntityManagerHandler entityManagerHandler = new EntityManagerHandler(entityManagerHelper);
-        return Proxy.newProxyInstance(EntityManager.class.getClassLoader(), new Class[]{EntityManager.class}, entityManagerHandler);
+                .forEach(method -> {
+                    Arrays.stream(method.getReturnType().getInterfaces()).forEach(iface -> beans.put(iface, method));
+                    beans.put(method.getReturnType(), method);
+                });
     }
 
     @Override
@@ -73,22 +53,53 @@ public class ApplicationContextImpl implements ApplicationContext {
         if (applicationScope.containsKey(clazz))
             return (T) applicationScope.get(clazz);
 
-        if (components.containsKey(clazz))
-            clazz = components.get(clazz);
+        Optional<Object> implementationClass = Optional.empty();
 
-        Object implementation = ClassUtil.newInstance(clazz);
-        injectDependencies(implementation);
-        addBeans(clazz, implementation);
-        return (T) implementation;
+        if (clazz.isAssignableFrom(EntityManager.class) && beans.containsKey(EntityManagerFactory.class)) {
+            implementationClass = Optional.of(createEntityManagerBean());
+        } else if (beans.containsKey(clazz) && appConfiguration.isPresent()) {
+            implementationClass = Optional.of(createBeanFromConfiguration(clazz));
+        } else if (components.containsKey(clazz) && appConfiguration.isPresent()) {
+            implementationClass = Optional.of(createBeanFromComponents(clazz));
+        }
+
+        implementationClass.orElseThrow(() -> new NullPointerException("No bean named '" + clazz.getName() + "' is defined"));
+        Object bean = implementationClass.get();
+        injectDependencies(bean);
+        addToApplicationScope(clazz, bean);
+        return (T) bean;
+    }
+
+    private <T> Object createBeanFromComponents(Class<T> clazz) {
+        Class component = components.get(clazz);
+        return ClassUtil.newInstance(component);
+    }
+
+    private <T> Object createBeanFromConfiguration(Class<T> clazz) {
+        Method method = beans.get(clazz);
+        return ClassUtil.invokeMethod(appConfiguration.get(), method);
+    }
+
+    private Object createEntityManagerBean() {
+        Method method = beans.get(EntityManagerFactory.class);
+        EntityManagerFactory entityManagerFactory = (EntityManagerFactory) ClassUtil.invokeMethod(appConfiguration.get(), method);
+        EntityManagerHelper entityManagerHelper = new EntityManagerHelper(entityManagerFactory);
+        EntityManagerHandler entityManagerHandler = new EntityManagerHandler(entityManagerHelper);
+        return Proxy.newProxyInstance(EntityManager.class.getClassLoader(), new Class[]{EntityManager.class}, entityManagerHandler);
     }
 
     private void injectDependencies(Object implementation) {
         Arrays.stream(implementation.getClass().getDeclaredFields())
-                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .filter(field -> isDepedencyInjectionAnnotation(field))
                 .forEach(field -> ClassUtil.setField(field, implementation, getBean(field.getType())));
     }
 
-    private void addBeans(Class<?> type, Object object) {
+    private boolean isDepedencyInjectionAnnotation(Field field) {
+        return field.isAnnotationPresent(Inject.class) || field.isAnnotationPresent(PersistenceContext.class);
+    }
+
+
+    private void addToApplicationScope(Class<?> type, Object object) {
         Arrays.stream(object.getClass().getInterfaces()).forEach(iface -> applicationScope.put(iface, object));
         applicationScope.put(type, object);
     }
