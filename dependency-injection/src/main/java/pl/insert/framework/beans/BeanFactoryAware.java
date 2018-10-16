934ff7de13b9853a4d8371715360f5dac1979b9c
KamilWro <kamil.breczko@gmail.com>
2018-10-16 23:26:34 +0200

    add interface BeanFactoryAware & field to enum

---
+++ dependency-injection/src/main/java/pl/insert/framework/beans/BeanFactoryAware.java
@@ -0,0 +1,6 @@
+package pl.insert.framework.beans;
+
+public interface BeanFactoryAware {
+
+    void setBeanFactory(BeanFactory beanFactory);
+}
--- dependency-injection/src/main/java/pl/insert/framework/transactional/enums/Propagation.java
+++ dependency-injection/src/main/java/pl/insert/framework/transactional/enums/Propagation.java
@@ -1,6 +1,16 @@
 package pl.insert.framework.transactional.enums;
 
 public enum Propagation {
-    REQUIRED,
-    REQUIRES_NEW
+    REQUIRED(false),
+    REQUIRES_NEW(true);
+
+    private boolean newTransactionRequired;
+
+    Propagation(boolean newTransactionRequired) {
+        this.newTransactionRequired = newTransactionRequired;
+    }
+
+    public boolean isNewTransactionRequired() {
+        return newTransactionRequired;
+    }
 }
